1c5cbe114c36634d92849bf110c73969cdcf3b95
stefanpython <stefanhtml@yahoo.com>
2023-11-06 17:34:10 +0200

    Fetch and display recent recipes on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Home />
+      <HashRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
 
 export default App;
+
+// Search if you can fetch the newest recipes and display them grid 3 on a row
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,47 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
 
 function Home() {
-  return <h1>Home</h1>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecentRecipes();
+  }, []);
+
+  const fetchRecentRecipes = () => {
+    fetch(
+      "https://api.spoonacular.com/recipes/complexSearch?apiKey=65fb4eb13c2745dc8613ec2119bbaa69&sort=created&number=9"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data from the server");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Assuming the response data is an array of recipes
+        setRecipes(data.results); // Get the most recent 9 recipes
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  console.log(recipes);
+
+  return (
+    <div className="home-container">
+      <h2>Recent Recipes</h2>
+      <div className="recipe-grid">
+        {recipes.map((recipe) => (
+          <div className="recipe-card" key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image} alt={recipe.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
