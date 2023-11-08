abe9b4ee8744da0c1126733bf91d260d3faad598
stefanpython <stefanhtml@yahoo.com>
2023-11-08 17:20:54 +0200

    Display recipes from search results

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,19 +1,20 @@
-import "./Recipes.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Recipes() {
   const location = useLocation();
   const searchQuery = new URLSearchParams(location.search).get("query");
   const [recipeData, setRecipeData] = useState([]);
+  const [page, setPage] = useState(10); // Track the current page
+  const isLoading = useRef(false);
 
-  useEffect(() => {
-    fetchSearch();
-  }, [searchQuery]);
-
-  const fetchSearch = () => {
+  const fetchSearch = (pageNum) => {
+    isLoading.current = true;
     fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&apiKey=65fb4eb13c2745dc8613ec2119bbaa69`
+      `https://api.spoonacular.com/recipes/complexSearch?query=${searchQuery}&apiKey=65fb4eb13c2745dc8613ec2119bbaa69&number=${pageNum}&offset=${
+        (pageNum - 1) * 10
+      }`
     )
       .then((response) => {
         if (!response.ok) {
@@ -22,22 +23,56 @@ function Recipes() {
         return response.json();
       })
       .then((data) => {
-        setRecipeData(data.results);
+        setRecipeData((prevData) => [...prevData, ...data.results]);
+        isLoading.current = false;
       })
       .catch((err) => {
         console.error("Error retrieving data: ", err);
+        isLoading.current = false;
       });
   };
 
-  console.log(recipeData);
+  const handleScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-  return <h1>Recipes page</h1>;
+    if (scrollTop + clientHeight >= scrollHeight - 10 && !isLoading.current) {
+      // Load more recipes when the user is 10 pixels from the bottom
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    fetchSearch(page);
+  }, [searchQuery, page]);
+
+  useEffect(() => {
+    // Add a scroll event listener to the document
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      // Remove the event listener when the component unmounts
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="recipe-grid">
+      {recipeData.map((recipe) => (
+        <div className="recipe-card" key={recipe.id}>
+          <Link to={`/recipe/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image} alt={recipe.title} />
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Recipes;
 
-// - display recipes
-// - make them clickable with link
+// - display recipes - done
+// - make them clickable with link - done
 // - display related recipes in single page
 // - style everithing up
 // - add back button to navbar
